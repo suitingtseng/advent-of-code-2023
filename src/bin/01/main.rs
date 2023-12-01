bf5ed9029e8edd2e43abcc7e76df4981fd2a89ed
Suiting Tseng <suitingtseng@gmail.com>
2023-12-01 23:24:35 +0000

    day 01: part 2

--- src/bin/01/main.rs
+++ src/bin/01/main.rs
@@ -1,5 +1,22 @@
 fn main() -> anyhow::Result<()> {
+    let words = vec![
+        ("one", "1"),
+        ("two", "2"),
+        ("three", "3"),
+        ("four", "4"),
+        ("five", "5"),
+        ("six", "6"),
+        ("seven", "7"),
+        ("eight", "8"),
+        ("nine", "9"),
+    ];
+
     let input = std::fs::read_to_string("input/01/01.txt")?;
+    let mut replaced_input = input.clone();
+    for (word, digit) in &words {
+        replaced_input =
+            replaced_input.replace(word, format!("{word}{digit}{word}{digit}{word}").as_str());
+    }
     let lines = input.lines();
     let mut result: u32 = 0;
     for line in lines {
