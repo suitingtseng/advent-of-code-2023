5d12fb91e2fde0321147137ad8a12168d9bd9a8e
Suiting Tseng <suitingtseng@gmail.com>
2023-12-03 23:08:02 +0000

    day 03: refactor

--- src/bin/03/main.rs
+++ src/bin/03/main.rs
@@ -1,11 +1,18 @@
 use std::collections::HashMap;
+
+#[derive(PartialEq, Eq, Hash)]
+struct Point {
+    row: i64,
+    col: i64,
+}
+
 fn main() -> anyhow::Result<()> {
     let input = std::fs::read_to_string("input/03/03.txt")?;
     let lines: Vec<&str> = input.lines().collect();
     let m = lines.len();
     let n = lines[0].len();
     let mut total: i64 = 0;
-    let mut gears: HashMap<(i64, i64), Vec<i64>> = HashMap::new();
+    let mut gears: HashMap<Point, Vec<i64>> = HashMap::new();
     for i in 0..m {
         let mut j = 0;
         while j < n {
@@ -13,28 +20,25 @@ fn main() -> anyhow::Result<()> {
             let c: char = line.as_bytes()[j].into();
             if !c.is_digit(10) {
                 j += 1;
-                continue;
             } else {
                 let start = j;
-                while j < n && char::from(line.as_bytes()[j]).is_digit(10) {
+                while j < n && line.as_bytes()[j].is_ascii_digit() {
                     j += 1;
                 }
                 let end = j;
-                let mut gear_neighbors = vec![i as i64]
+                let gear_neighbors = vec![i as i64]
                     .into_iter()
-                    .map(|i| vec![i - 1, i, i + 1])
-                    .flatten()
-                    .map(|i| ((start as i64 - 1)..(end as i64 + 1)).map(move |j| (i, j)))
-                    .flatten()
-                    .filter(|tup| {
-                        tup.0 >= 0
-                            && tup.0 < m as i64
-                            && tup.1 >= 0
-                            && tup.1 < n as i64
-                            && !(tup.0 == i as i64 && tup.1 >= start as i64 && tup.1 < end as i64)
+                    .flat_map(|row| vec![row - 1, row, row + 1])
+                    .flat_map(|row| {
+                        ((start as i64 - 1)..(end as i64 + 1)).map(move |col| Point { row, col })
                     })
-                    .filter(|tup| {
-                        char::from(lines[tup.0 as usize].as_bytes()[tup.1 as usize]) == '*'
+                    .filter(|p| {
+                        p.row >= 0
+                            && p.row < m as i64
+                            && p.col >= 0
+                            && p.col < n as i64
+                            && !(p.row == i as i64 && p.col >= start as i64 && p.col < end as i64)
+                            && lines[p.row as usize].as_bytes()[p.col as usize] == b'*'
                     });
                 let number = line[start..end].parse::<i64>().unwrap();
                 for tup in gear_neighbors {
@@ -47,8 +51,6 @@ fn main() -> anyhow::Result<()> {
         }
     }
 
-    // 12 red cubes, 13 green cubes, and 14 blue cubes
-
     for v in gears.values() {
         if v.len() == 2 {
             total += v[0] * v[1];
@@ -56,8 +58,4 @@ fn main() -> anyhow::Result<()> {
     }
     println!("total: {total}");
     Ok(())
-}
-
-fn is_symbol(c: char) -> bool {
-    c != '.' && !c.is_digit(10)
 }
